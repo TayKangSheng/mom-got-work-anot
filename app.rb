183093abe894acd9b3be1f3b19b763f7e2a03ffd
Tay Kang Sheng <kangsheng@gmail.com>
2017-12-31 15:32:41 +0800

    add api

--- app.rb
+++ app.rb
@@ -9,29 +9,43 @@ require "date"
 require "slim"
 
 get "/" do
-  today = Time.now.getlocal("+08:00").to_date
-
-  dates = get_file(today.year)
-  @message = "No data found." if dates.nil?
-
-  dates["holidays"].each do |d|
-    holiday_date = Date.parse(d["date"])
-
-    @message = "No. Its #{d["name"]}" if holiday_date == today
-
-    if holiday_date.sunday?
-      @message = "No. Its #{d["name"]} in lieu!" if (holiday_date + 1) == today
-    end
-  end
-
-  @message = "No. Its Saturday!" if today.saturday?
-  @message = "No. Its Sunday!" if today.sunday?
-
-  @message = "Yes! Go to work!" if @message.empty?
+  @message = get_status_today
 
   slim :base, layout: false do
     slim :index
   end
+end
+
+get "/api" do
+  slim :base, layout: false do
+    slim :api
+  end
+end
+
+get "/api/" do
+  { message: get_status_today }.to_json
+end
+
+def get_status_today
+  today = Time.now.getlocal("+08:00").to_date
+
+  dates = get_file(today.year)
+  return "No data found." if dates.nil?
+
+  dates["holidays"].each do |d|
+    holiday_date = Date.parse(d["date"])
+
+    return "No. Its #{d["name"]}" if holiday_date == today
+
+    if holiday_date.sunday?
+      return "No. Its #{d["name"]} in lieu!" if (holiday_date + 1) == today
+    end
+  end
+
+  return "No. Its Saturday!" if today.saturday?
+  return "No. Its Sunday!" if today.sunday?
+
+  return "Yes! Go to work!" if @message.empty?
 end
 
 def get_file(year)
